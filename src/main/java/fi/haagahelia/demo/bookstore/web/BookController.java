4a521d11337440677f090b0fbb480830a9014b0b
wes337 <wesmoses@gmail.com>
2018-09-07 15:21:27 +0300

    Added list page, add books

--- src/main/java/fi/haagahelia/demo/bookstore/web/BookController.java
+++ src/main/java/fi/haagahelia/demo/bookstore/web/BookController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import fi.haagahelia.demo.bookstore.domain.Book;
 import fi.haagahelia.demo.bookstore.domain.BookRepository;
 
 @Controller
@@ -15,5 +17,23 @@ public class BookController {
 	@RequestMapping(value="/index")
 	public String Bookstore(Model model) {
 		return "index";
+	}
+	
+	@RequestMapping(value="/booklist")
+	public String bookList(Model model) {
+		model.addAttribute("books", repository.findAll());
+		return "booklist";
+	}
+	
+	@RequestMapping(value="/add")
+	public String addBook(Model model) {
+		model.addAttribute("book", new Book());
+		return "addbook";
+	}
+	
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String save(Book book) {
+		repository.save(book);
+		return "redirect:booklist";
 	}
 }
