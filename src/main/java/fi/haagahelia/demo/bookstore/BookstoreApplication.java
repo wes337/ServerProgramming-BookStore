f474473a5b6e5c3d11fbd7f4cdcc437d2840c578
wes337 <wesmoses@gmail.com>
2018-10-05 13:34:49 +0300

    Added more test books with CommandLineRunner

--- src/main/java/fi/haagahelia/demo/bookstore/BookstoreApplication.java
+++ src/main/java/fi/haagahelia/demo/bookstore/BookstoreApplication.java
@@ -26,6 +26,14 @@ public class BookstoreApplication {
 			
 			Book b1 = new Book("The Witch", "Camilla Lackberg", "38949029-3", 2017, 10, crepository.findByName("Fantasy").get(0));
 			repository.save(b1);
+			Book b2 = new Book("Animal Farm", "George Orwell", "045228424-4", 1945, 10, crepository.findByName("Fiction").get(0));
+			repository.save(b2);
+			Book b3 = new Book("The Chrysalids", "John Wyndham", "159017292-2", 1955, 10, crepository.findByName("Science Fiction").get(0));
+			repository.save(b3);
+			Book b4 = new Book("All The Light We Cannot See", "Anthony Doerr", "8147674658-6", 2014, 10, crepository.findByName("Historical Fiction").get(0));
+			repository.save(b4);
+			Book b5 = new Book("The Dark Tower: The Gunslinger", "Stephen King", "9150116180-3", 1982, 10, crepository.findByName("Fantasy").get(0));
+			repository.save(b5);
 		};
 	}
 }
