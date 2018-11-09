f7b9c3fe1e99fd052805ba10fe6293fa0c6cca43
wes337 <wesmoses@gmail.com>
2018-11-09 13:41:49 +0200

    smoke test for controller

--- src/test/java/fi/haagahelia/demo/bookstore/BookstoreApplicationTests.java
+++ src/test/java/fi/haagahelia/demo/bookstore/BookstoreApplicationTests.java
@@ -1,16 +1,25 @@
 package fi.haagahelia.demo.bookstore;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import fi.haagahelia.demo.bookstore.web.BookController;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BookstoreApplicationTests {
+	
+	@Autowired
+	private BookController controller;
 
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
 	}
 
 }
